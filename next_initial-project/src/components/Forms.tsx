43310e58f76222a3b2b93ffcb4d91e310bd873c8
phfuark <paulohsilva2007@gmail.com>
2024-12-26 20:25:48 -0300

    26.12.2024 20:26

---
+++ next_initial-project/src/components/Forms.tsx
@@ -0,0 +1,16 @@
+import { Circle } from "./Circle";
+import { Square } from "./Square";
+
+export const Forms = () => {
+    return (
+        <div className="
+            flex
+            gap-4   
+        ">
+            <Square/>
+            <Circle/>
+        </div>
+    );
+}
+
+
